b1ff804adb297bc6c4de91e2c289ff7223952e68
Gabriel Paiva De Almeida <56406122+GABRIELPA2@users.noreply.github.com>
2019-10-16 23:41:04 -0300

    Update Blackjack_EP12019.py

--- Blackjack_EP12019.py
+++ Blackjack_EP12019.py
@@ -20,6 +20,7 @@ def retorna_carta(lista_cartas):
     return carta
  
 def dealer():
+ 
     cartadealer1=retorna_carta(cartas_embaralhadas)
     cartadealer2=retorna_carta(cartas_embaralhadas)
     somadealer=cartadealer1+cartadealer2
@@ -44,62 +45,3 @@ if baralhos<1 or baralhos>4:
     print('Quantidade de baralhos não disponível, jogo definido com 1 baralho')
 fichas=3
  
-while play_again=='s' and fichas>0 and baralhos<=4 and baralhos>0:
-    fichas-=1
-    valor_apostado=0
-    J = 10
-    Q = 10
-    K = 10
-    A = 1
-    deck_de_cartas = [2,3,4,5,6,7,8,9,10,J,Q,K,A]*4*baralhos
-    
-    valor_apostado=input('Você possui {0} dólares. Qual será o valor da sua aposta? (mínimo: 15) '.format(saldo))
-    
-    if valor_apostado=='fim' or valor_apostado=='Fim':
-        break
-    
-    valor_apostado=int(valor_apostado)
-    
-    if valor_apostado>saldo or valor_apostado<15:
-        print('Valor escolhido fora de alcance.')
-        valor_apostado=input('Você possui {0} dólares. Qual será o valor da sua aposta? (mínimo: 15) '.format(saldo))
-        if valor_apostado=='fim' or valor_apostado=='Fim':
-            print('Fim do jogo')
-            break
-         else: 
-        saldo=saldo-valor_apostado
-        cartas_embaralhadas=(embaralha_cartas(deck_de_cartas))
-        carta1=retorna_carta(cartas_embaralhadas)
-        carta2=retorna_carta(cartas_embaralhadas)
-        soma_de_cartas=carta1+carta2
-        if carta1==A:
-            if soma_de_cartas+10<=21:
-                carta1=11
-                soma_de_cartas+=10
-        if carta2==A:
-            if soma_de_cartas+10<=21:
-                carta2=11
-                soma_de_cartas+=10
-        print('Suas cartas iniciais são: {0} e {1} (soma: {2})'.format(carta1,carta2,soma_de_cartas))
-        
-        if soma_de_cartas==21:
-            madness=madness()
-            saldo+=(valor_apostado*2.5)+madness
-            fichas+=2
-            print('Você ganhou o jogo (BLACKJACK), mais duas fichas e mais {0} dólares pelo sorteio MADNESS.'.format(madness))
-            print('Seu saldo é agora de {0}'.format(saldo))
-            
-        while soma_de_cartas<21:
-            escolha=input('Você deseja parar ou continuar?(p/c) ')
-            clear()
-            aposta_extra=input('Aceita aumentar sua aposta? (s/n) (saldo: {0}) '.format(saldo))
-            if aposta_extra=='s' or aposta_extra=='S':
-                aumento=int(input('Qual será o valor do aumento? '))
-                if aumento<=saldo:
-                    saldo-=aumento
-                    valor_apostado+=aumento
-                else:
-                    print('Aumento de aposta inválido')
-            input('Saldo atual: {0} // Aposta atual: {1} (aperte enter/return)\n'.format(saldo,valor_apostado))
-            if escolha=='p' or escolha=='P':
-                cartas_dealer=dealer()
