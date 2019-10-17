5f5d51d82c361bea2da34ed3dbe9a99b21cfd867
PedroRubens <56384627+PedroRubens@users.noreply.github.com>
2019-10-16 22:49:31 -0300

    Update Blackjack_EP12019.py

--- Blackjack_EP12019.py
+++ Blackjack_EP12019.py
@@ -33,3 +33,36 @@ def dealer():
 def madness():
     sorteio = random.randint(5,100)
     return sorteio
+ 
+saldo_inicial=100
+saldo=saldo_inicial
+play_again='s'
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n **Você possui 3 fichas. Cada jogo gastará 1 delas**')
+baralhos=int(input('\n Com quantos baralhos você deseja jogar? (1-4) '))
+if baralhos<1 or baralhos>4:
+    baralhos=1
+    print('Quantidade de baralhos não disponível, jogo definido com 1 baralho')
+fichas=3
+ 
+while play_again=='s' and fichas>0 and baralhos<=4 and baralhos>0:
+    fichas-=1
+    valor_apostado=0
+    J = 10
+    Q = 10
+    K = 10
+    A = 1
+    deck_de_cartas = [2,3,4,5,6,7,8,9,10,J,Q,K,A]*4*baralhos
+    
+    valor_apostado=input('Você possui {0} dólares. Qual será o valor da sua aposta? (mínimo: 15) '.format(saldo))
+    
+    if valor_apostado=='fim' or valor_apostado=='Fim':
+        break
+    
+    valor_apostado=int(valor_apostado)
+    
+    if valor_apostado>saldo or valor_apostado<15:
+        print('Valor escolhido fora de alcance.')
+        valor_apostado=input('Você possui {0} dólares. Qual será o valor da sua aposta? (mínimo: 15) '.format(saldo))
+        if valor_apostado=='fim' or valor_apostado=='Fim':
+            print('Fim do jogo')
+            break
