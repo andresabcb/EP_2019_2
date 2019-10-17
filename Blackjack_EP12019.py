19520e0a0d9128a6c1264a94da1bd203d27048c6
PedroRubens <56384627+PedroRubens@users.noreply.github.com>
2019-10-16 21:13:41 -0300

    Update Blackjack_EP12019.py

---
+++ Blackjack_EP12019.py
@@ -0,0 +1,26 @@
+import random
+ 
+ 
+def embaralha_cartas(lista):
+    cartas=lista[:]
+    random.shuffle(cartas)
+    return cartas
+ 
+def retorna_carta(lista_cartas):
+    carta=lista_cartas[0]
+    del(lista_cartas[0])
+    return carta
+ 
+def dealer():
+    cartadealer1=retorna_carta(cartas_embaralhadas)
+    cartadealer2=retorna_carta(cartas_embaralhadas)
+    somadealer=cartadealer1+cartadealer2
+    print('As cartas do dealer são: {0} e {1} (soma: {2})'.format(cartadealer1,cartadealer2,somadealer))
+    while somadealer<=17:
+        cartanova=retorna_carta(cartas_embaralhadas)
+        somadealer+=cartanova
+        print('{0} (soma: {1})'.format(cartanova,somadealer))
+    return somadealer
+ 
+def madness():
+    sorteio = random.randint(5,100)
